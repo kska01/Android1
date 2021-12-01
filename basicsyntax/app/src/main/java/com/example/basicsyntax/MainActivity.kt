87760ce98df23a33805814e5bcb3e4badf1bb662
ka <insideka01@gmail.com>
2021-12-02 00:38:49 +0900

    12.2 0:38 ch03 코틀린 사용을 위한 기본 문법 완료

--- basicsyntax/app/src/main/java/com/example/basicsyntax/MainActivity.kt
+++ basicsyntax/app/src/main/java/com/example/basicsyntax/MainActivity.kt
@@ -34,5 +34,62 @@ class MainActivity : AppCompatActivity() {
         for (month in array) {
             Log.d("For", "현재 월은 ${month}입니다.")
         }
+
+    // InheritanceAndExtension 프로젝트: 상속과 확장
+
+        // 1. 부모 클래스 직접 호출하기
+        var parent = Parent()
+        parent.sayHello()
+        // 2. 자식 클래스 호출해서 사용하기
+        var child = Child()
+        child.myHello()
+
+        testStringExtension()
     }
+
+    // String 익스텐션 테스트 하기
+    fun testStringExtension() {
+        var original = "Hello"
+        var added = " Guys~"
+        // plus 메서드를 사용해서 문자열을 더할 수 있다
+        Log.d("Extension", "added를 더한 값은 ${original.plus(added)}입니다.")
+    }
+}
+
+// 상속 연습
+open class Parent {
+    var hello: String = "안녕하세요."
+    fun sayHello() {
+        Log.d("Extension", "${hello}")
+    }
+}
+class Child: Parent() {
+    fun myHello() {
+        hello = "Hello"
+        sayHello()
+    }
+}
+// 메서드 오버라이드 연습
+open class BaseClass {
+    open fun opened() {
+
+    }
+    fun notOpened() {
+
+    }
+}
+class ChildClass: BaseClass() {
+    override fun opened() {
+
+    }
+}
+// 프로퍼티 오버라이드 연습
+open class BaseClass2 {
+    open var opened: String = "i am"
+}
+class ChildClass2: BaseClass2() {
+    override var opened: String = "You are"
+}
+fun String.plus(word: String): String {
+    return this + word
 }
