344a305fb281148619c37822002bbcad87ef3b35
ka <insideka01@gmail.com>
2021-12-03 16:45:06 +0900

    12.3 ch4 2.9 시크바

---
+++ WidgetsSeekBar/app/src/main/java/com/example/widgetsseekbar/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.widgetsseekbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import com.example.widgetsseekbar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                binding.textView.text = "$p1"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+    }
+}
