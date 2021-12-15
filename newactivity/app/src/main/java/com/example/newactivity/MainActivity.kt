1d75caa538490a07c6ca41303f0123956f16316f
ka <insideka01@gmail.com>
2021-12-15 20:11:41 +0900

    12.15 ch5 1.3 새 액티비티 만들고 실행하기

---
+++ newactivity/app/src/main/java/com/example/newactivity/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.newactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.newactivity.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val intent = Intent(this, SubActivity::class.java)
+
+        binding.btnStart.setOnClickListener { startActivity(intent) }
+    }
+}
