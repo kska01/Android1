67244fde0fb4c5d0b66e4c05cf6c8203f19d5621
ka <insideka01@gmail.com>
2021-12-22 21:57:56 +0900

    12.22 SignUpAtctivity의 액션바 뒤로가기 버튼까지 적용

--- Centennial2/app/src/main/java/com/example/centennial2/LoginActivity.kt
+++ Centennial2/app/src/main/java/com/example/centennial2/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.centennial2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.centennial2.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+     val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        val intent = Intent(this, SignUpActivity::class.java)
+        binding.signUpBtn.setOnClickListener { startActivity(intent) }
     }
 }
---
+++ Centennial2/app/src/main/java/com/example/centennial2/SignUpActivity.kt
@@ -0,0 +1,17 @@
+package com.example.centennial2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.centennial2.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivitySignUpBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+    }
+}
