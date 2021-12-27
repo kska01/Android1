f394ed7370ec68ef852b0f07f0d2140958999a05
ka <insideka01@gmail.com>
2021-12-27 23:13:29 +0900

    12.27 ch5 recyclerview 목록 클릭 이벤트 처리

--- ContainerRecyclerView/app/src/main/java/com/example/containerrecyclerview/CustomAdapter.kt
+++ ContainerRecyclerView/app/src/main/java/com/example/containerrecyclerview/CustomAdapter.kt
@@ -2,6 +2,7 @@ package com.example.containerrecyclerview
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.containerrecyclerview.databinding.ItemRecyclerBinding
 import java.text.SimpleDateFormat
@@ -24,6 +25,12 @@ class CustomAdapter: RecyclerView.Adapter<Holder>() {
 }
 
 class Holder(val binding: ItemRecyclerBinding): RecyclerView.ViewHolder(binding.root) {
+    init {
+        binding.root.setOnClickListener {
+            Toast.makeText(binding.root.context, "클릭된 아이템 = ${binding.textTitle.text}", Toast.LENGTH_LONG).show()
+        }
+    }
+
     fun setMemo(memo: Memo) {
         binding.textNo.text = "${memo.no}"
         binding.textTitle.text = memo.title
