7c9e4bc87934c97edde2c819c9779d3c395590e9
ka <insideka01@gmail.com>
2022-01-04 16:26:06 +0900

    01.04 ch5 3.1 - 액티비티에 프래그먼트 추가하기

--- Fragment/app/src/main/java/com/example/fragment/MainActivity.kt
+++ Fragment/app/src/main/java/com/example/fragment/MainActivity.kt
@@ -7,5 +7,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setFragment()
+    }
+
+    //프래그먼트 삽입 함수
+    fun setFragment() {
+        val listFragment: ListFragment = ListFragment()
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.frameLayout, listFragment)
+        transaction.commit()
     }
 }
