e10ee7ab67d27ec30f4577976892a7a297c5fee0
ka <insideka01@gmail.com>
2022-01-05 11:24:20 +0900

    01.04 ch5 3.2 프래그먼트 화면 전환

--- Fragment/app/src/main/java/com/example/fragment/DetailFragment.kt
+++ Fragment/app/src/main/java/com/example/fragment/DetailFragment.kt
@@ -6,23 +6,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.fragment.databinding.FragmentListBinding
+import com.example.fragment.databinding.FragmentDetailBinding
 
-class ListFragment : Fragment() {
-    var mainActivity: MainActivity? = null
+
+class DetailFragment : Fragment() {
+    lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding =  FragmentListBinding.inflate(inflater, container, false)
-        binding.btnNext.setOnClickListener { mainActivity?.goDetail() }
+        val binding = FragmentDetailBinding.inflate(inflater, container, false)
+        binding.btnback.setOnClickListener { mainActivity.goBack() }
         return binding.root
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
-        if (context is MainActivity) mainActivity = context
+        mainActivity = context as MainActivity
     }
 }
--- Fragment/app/src/main/java/com/example/fragment/MainActivity.kt
+++ Fragment/app/src/main/java/com/example/fragment/MainActivity.kt
@@ -11,12 +11,26 @@ class MainActivity : AppCompatActivity() {
         setFragment()
     }
 
-    //프래그먼트 삽입 함수
+    // 리스트 프래그먼트 삽입 함수
     fun setFragment() {
-//        val listFragment: ListFragment = ListFragment()
-//
-//        val transaction = supportFragmentManager.beginTransaction()
-//        transaction.add(R.id.frameLayout, listFragment)
-//        transaction.commit()
+        val listFragment: ListFragment = ListFragment()
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.frameLayout, listFragment)
+        transaction.commit()
+    }
+
+    // 디테일 프래그먼트 삽입 함수
+    fun goDetail() {
+        val detailFragment = DetailFragment()
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.frameLayout, detailFragment)
+        transaction.addToBackStack("detail")
+        transaction.commit()
+    }
+
+    fun goBack() {
+        onBackPressed()
     }
 }
