ba2513ed450ac537b0fd01d27e1902357f110479
ka <insideka01@gmail.com>
2022-01-20 22:15:07 +0900

    01.20 bottom navigation 진행중

---
+++ MainPageOfCentennial/app/src/main/java/com/example/mainpageofcentennial/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.mainpageofcentennial
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mainpageofcentennial.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    val vinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(vinding.root)
+
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.item2 -> {
+                    val intent = Intent(this, CartActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.item3 -> {
+                    val intent = Intent(this, My100Activity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
---
+++ MainPageOfCentennial/app/src/main/java/com/example/mainpageofcentennial/My100Activity.kt
@@ -0,0 +1,11 @@
+package com.example.mainpageofcentennial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class My100Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my100)
+    }
+}
