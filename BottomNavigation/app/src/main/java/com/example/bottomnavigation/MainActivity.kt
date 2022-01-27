0b43e421b5cd75e0bbcf3fa8b1beafdd30ebd6f0
ka <insideka01@gmail.com>
2022-01-27 12:13:30 +0900

    01.27 bottomnavigation + fragment + viewpaer + tablayout, 프레그먼트에 올린 viewpager에 fragment를 또 올리는 중

--- BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.bottomnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import com.example.bottomnavigation.databinding.ActivityMainBinding
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val fl: FrameLayout by lazy {
+        findViewById(R.id.frameLayout)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+
+        bottomNavigation.setOnItemSelectedListener { item ->
+            changeFragment(
+                when (item.itemId) {
+                    R.id.menu -> MenuFragment()
+                    R.id.cart -> CartFragment()
+                    else -> My100Fragment()
+                }
+            )
+            true
+        }
+        bottomNavigation.selectedItemId = R.id.menu
+
+        // MenuFragment Adapter
+        val fragmentList = listOf(CrabFragment(), ShrimpFragment(), OysterFragment(), JeotgalFragment(), ChoiceFragment())
+        val adapter = FragmentAdapter(this)
+        adapter.fragmentList = fragmentList
+        val menuFragment = MenuFragment()
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameLayout, fragment)
+            .commit()
     }
 }
